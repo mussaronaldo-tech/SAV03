51ad13b40c93cf8758d34ec135816b60a6531649
mussaronaldo-tech <mussa_ronaldo@hotmail.es>
2025-10-25 16:24:21 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Bienvenido a SAV03 🚀</h1><p>Tu panel está activo.</p>"
+
+@app.route("/admin")
+def admin():
+    return "<h2>Panel Admin de SAV03</h2><p>Funcionalidad próximamente.</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
